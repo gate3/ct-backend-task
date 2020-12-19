121ec6a75b140cf96d8969a24cbbd8a8fde5ec95
Doyin Olarewaju <doyinolarewaju@gmail.com>
2020-12-20 00:39:38 +0100

    Return environment variables from validation function

--- config/env-variables.js
+++ config/env-variables.js
@@ -1,24 +1,15 @@
 const Joi = require('joi');
-const {
-    FIREBASE_SERVICE_ACCOUNT_BASE_64_KEY,
-    FIREBASE_DATABASE_URL
-} = process.env;
 
 const requiredStringRule = Joi.string().required();
 // This line provides an error message for when a variable isn't set
 const getVariableNotFoundErrorMessage = property => new Error(`${property} - was not found in the environment variables.`);
 
-module.exports = async () => {
+module.exports = () => {
     const schema = Joi.object({
         FIREBASE_SERVICE_ACCOUNT_BASE_64_KEY: requiredStringRule.error(getVariableNotFoundErrorMessage('FIREBASE_SERVICE_ACCOUNT_BASE_64_KEY')),
         FIREBASE_DATABASE_URL: requiredStringRule.error(getVariableNotFoundErrorMessage('FIREBASE_DATABASE_URL')),
     }).unknown(true);
 
-    await schema.validateAsync(process.env);
-
-    return {
-        firebaseServiceAccountBase64Key: FIREBASE_SERVICE_ACCOUNT_BASE_64_KEY,
-        firebaseDatabaseUrl: FIREBASE_DATABASE_URL
-    }
+    return schema.validateAsync(process.env)
 };
 
