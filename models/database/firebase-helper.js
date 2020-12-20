37bc0e05e26d209066ef4e617abd642a9798e927
Doyin Olarewaju <doyinolarewaju@gmail.com>
2020-12-20 03:32:12 +0100

    feat(ct-38): Firebase Specific Implementation - Create firebase connection object - Create database loader to create a connection to the database or fail - Firebase helper class with generic functions - Remove .gitkeep from models

---
+++ models/database/firebase-helper.js
@@ -0,0 +1,47 @@
+/**
+ * class
+ * FirebaseHelper class contains utitlity functions that are specific to firebase.
+ */
+class FirebaseHelper {
+  /*
+   * Constructor Function
+   * @param database Connection Object
+   */
+  constructor({database}) {
+    this._database = database();
+  }
+
+  /*
+   * @param modelName - Name of the model to return
+   * @returns {A firebase Model object}
+   */
+  getModel(modelName) {
+    return this._database.ref().child(modelName);
+  }
+
+  /*
+   *
+   * @param model - The model/collection we want to create a new entity in.
+   * @param newEntity - The new data to be created
+   * @returns {object}
+   */
+  async saveEntity({model, newEntity}) {
+    const pushId = model.push(newEntity).key();
+    return {
+      ...newEntity,
+      id: pushId,
+    };
+  }
+
+  /**
+   * This performs a full update and not a partial one. It completely replaces the object.
+   * @param model - The model/collection we want to create a new entity in.
+   * @param newEntity - The new data to be created
+   * @returns {object}
+   */
+  updateEntityFull({model, updatedEntity}) {
+    return model.set(updatedEntity);
+  }
+}
+
+module.exports = FirebaseHelper;
