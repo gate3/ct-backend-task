43ef6e9d55167c8f4f7aa2f1aca0d721e8f34599
Doyin Olarewaju <doyinolarewaju@gmail.com>
2020-12-20 19:18:19 +0100

    feat(ct-52): Order Repository Unit Tests - Unit test for the order repository create and update operations - Create mock objects for the firebase helper class - Modify order repository updatedEntity variable name - Create mock order and mockId

---
+++ models/tests/order-repository.test.js
@@ -0,0 +1,105 @@
+const FirebaseHelper = require('../database/firebase-helper');
+jest.mock('../database/firebase-helper');
+
+const OrderRepository = require('../orders-repository');
+const mockSampleOrder = {
+  address: {
+    city: 'Lagos',
+    country: 'Nigeria',
+    street: '1, Twins Obasa Street',
+    zip: '23401',
+  },
+  bookingDate: new Date().getTime(),
+  customer: {
+    email: 'doyin@gmail.com',
+    name: 'Oluwadoyin Olarewaju',
+    phone: '+2348130309625',
+  },
+  title: 'Block of Flats',
+};
+describe('Order Repository Class', () => {
+  afterEach(() => jest.clearAllMocks());
+  // Arrange
+  const firebaseHelper = new FirebaseHelper({database: jest.fn()});
+  const orderId = '-MOzZMdI0rVbERZSeu4Q';
+  const saveEntityMock = jest.fn(async ({model, newEntity}) => ({
+    ...newEntity,
+    id: orderId,
+  }));
+  const mockModel = {};
+  const getModelMock = jest.fn(() => mockModel);
+  const updateFullEntityMock = jest.fn();
+  firebaseHelper.getModel
+      .mockImplementation(getModelMock);
+  firebaseHelper.saveEntity
+      .mockImplementation(saveEntityMock);
+  firebaseHelper.updateFullEntity
+      .mockImplementation(updateFullEntityMock);
+
+  const orderRepository = new OrderRepository({databaseHelper: firebaseHelper});
+
+  describe('Create order function', () => {
+    // eslint-disable-next-line max-len
+    it('should call the database helper saveEntity function and return the savedOrder with a pushId.', async () => {
+      // Arrange
+      const expectedOrder = {
+        ...mockSampleOrder,
+        id: orderId,
+      };
+      // Act
+      const actualOrder = await orderRepository.createOrder({
+        newOrder: mockSampleOrder,
+      });
+
+      // Assert
+      expect(actualOrder).toMatchObject(expectedOrder);
+    });
+    // eslint-disable-next-line max-len
+    it('should call the saveEntity function of firebaseHelper with the right arguments.', async () => {
+      // Arrange
+
+      // Act
+      await orderRepository.createOrder({
+        newOrder: mockSampleOrder,
+      });
+
+      // Assert
+      expect(saveEntityMock).toHaveBeenCalledWith({
+        model: mockModel,
+        newEntity: mockSampleOrder,
+      });
+    });
+  });
+
+  describe('Update order function', () => {
+    // eslint-disable-next-line max-len
+    it('should call the database helper updateFullEntity function and not expect a return value', async () => {
+      // Arrange
+      const expectedResult = null;
+      // Act
+      const actualResult = await orderRepository.updateOrder({
+        orderId,
+        newOrder: mockSampleOrder,
+      });
+
+      // Assert
+      expect(actualResult).toMatchObject(expectedResult);
+    });
+    // eslint-disable-next-line max-len
+    it('should call the updateFullEntity function of firebaseHelper with the right arguments.', async () => {
+      // Arrange
+
+      // Act
+      await orderRepository.updateOrder({
+        orderId,
+        updatedOrder: mockSampleOrder,
+      });
+
+      // Assert
+      expect(updateFullEntityMock).toHaveBeenCalledWith({
+        model: mockModel,
+        updatedEntity: mockSampleOrder,
+      });
+    });
+  });
+});
