191f2a0cfd4422036113b59fc5ca9178b66f2afb
Doyin Olarewaju <doyinolarewaju@gmail.com>
2020-12-21 03:20:39 +0100

    feat(ct-30): Update order endpoint - Implement a route for updating an order - Add orderId parameter to constants file

--- routes/orders.js
+++ routes/orders.js
@@ -2,18 +2,33 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 const DI = require('../di-container');
+const responseHelper = DI.container.resolve('responseHelper');
 
-const {VERSIONS, PATHS} = require('./route-constants');
-const createOrderRoute = async (req, res) => {
-  const responseHelper = DI.container.resolve('responseHelper');
+const {VERSIONS, PATHS, PARAMETERS} = require('./route-constants');
+const createOrderRoute = async (request, response) => {
   try {
-    const createdOrder = await DI.container.resolve('createOrderService')(req);
+    const createdOrder = await DI.container.resolve(
+        'createOrderService',
+    )(request);
     return responseHelper.successResponse(res, createdOrder);
   } catch (e) {
-    console.log(e);
     return responseHelper.errorResponse(res, e);
   }
 };
 router.post(`${VERSIONS.V1}${PATHS.ORDERS}`, createOrderRoute);
+
+const updateOrderRoute = async (request, response) => {
+  try {
+    const createdOrder = await DI.container.resolve(
+        'updateOrderService',
+    )(request);
+    return responseHelper.successResponse(response, createdOrder);
+  } catch (e) {
+    return responseHelper.errorResponse(response, e);
+  }
+};
+router.put(
+    `${VERSIONS.V1}${PATHS.ORDERS}${PARAMETERS.ORDER_ID}`, updateOrderRoute,
+);
 
 module.exports = router;
